95ff9c026a354f2e92848b6ce3ba1d4d6a056cf5
Rachel Goh <rachelg0129@gmail.com>
2022-12-03 15:09:10 -0500

    route setup

---
+++ src/app/body/habit-tracker/mood-selector/mood-selector.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mood-selector',
+  templateUrl: './mood-selector.component.html',
+  styleUrls: ['./mood-selector.component.scss']
+})
+export class MoodSelectorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
