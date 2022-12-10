17cdd1c23bea23bedebb9dc86699d794c84c28a7
Rachel Goh <rachelg0129@gmail.com>
2022-12-10 17:21:23 -0500

    auth component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './auth/register/register.component';
 import { ContactUsComponent } from './body/contact-us/contact-us.component';
+import { AddEditItemComponent } from './body/habit-tracker/add-edit-item/add-edit-item.component';
 import { HabitTrackerComponent } from './body/habit-tracker/habit-tracker.component';
 import { HomeComponent } from './body/home/home.component';
 
@@ -12,7 +14,9 @@ const routes: Routes = [
   },
   { path: 'contact', component: ContactUsComponent },
   { path: 'habit-tracker', component: HabitTrackerComponent },
-  // // { path: 'details/:id', component:  },
+  { path: 'new', component: AddEditItemComponent },
+  { path: 'edit/:id', component: AddEditItemComponent },
+  { path: 'register', component: RegisterComponent },
 ];
 
 @NgModule({
