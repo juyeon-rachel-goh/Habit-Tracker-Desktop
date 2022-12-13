d067387effc829f60b0d51708e3ac3feadfaed53
Rachel Goh <rachelg0129@gmail.com>
2022-12-13 16:01:08 -0500

    homepage setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './auth/register/register.component';
-import { ContactUsComponent } from './body/contact-us/contact-us.component';
-import { AddEditItemComponent } from './body/habit-tracker/add-edit-item/add-edit-item.component';
-import { HabitTrackerComponent } from './body/habit-tracker/habit-tracker.component';
-import { HomeComponent } from './body/home/home.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  // { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   {
     path: 'home',
     component: HomeComponent,
   },
-  { path: 'contact', component: ContactUsComponent },
-  { path: 'habit-tracker', component: HabitTrackerComponent },
-  { path: 'new', component: AddEditItemComponent },
-  { path: 'edit/:id', component: AddEditItemComponent },
   { path: 'register', component: RegisterComponent },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,44 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import { BodyModule } from './body/body.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MoodSelectorComponent } from './body/habit-tracker/mood-selector/mood-selector.component';
-import { RegisterComponent } from './auth/register/register.component';
 import { InputModule } from './input/input.module';
 import { AuthModule } from './auth/auth.module';
 import { PipesModule } from './pipes/pipes.module';
 import { HttpClientModule } from '@angular/common/http';
+import { LayoutModule } from './layout/layout.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    MoodSelectorComponent,
-  ],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    NgbModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    BodyModule,
     AuthModule,
     InputModule,
     PipesModule,
     HttpClientModule,
+    LayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
