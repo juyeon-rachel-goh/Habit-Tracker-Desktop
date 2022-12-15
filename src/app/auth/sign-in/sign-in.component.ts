2ac45cbea350a41da8793417926e1a8edd68034b
Rachel Goh <rachelg0129@gmail.com>
2022-12-15 16:06:11 -0500

    login page initial setup

--- src/app/auth/sign-in/sign-in.component.ts
+++ src/app/auth/sign-in/sign-in.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.scss']
+  styleUrls: ['./sign-in.component.scss'],
 })
 export class SignInComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(
+    private authService: AuthService,
+    private formBuilder: FormBuilder
+  ) {}
+  public registerForm = new FormGroup({});
+  public showPassword: boolean = false;
+  public passwordType = 'text';
+  public passwordIcon = 'visibility_off';
   ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      username: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
   }
 
+  onSubmit() {
+    console.log(this.registerForm.value);
+    this.authService.addUser(this.registerForm.value).subscribe();
+  }
+
+  public togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
+    this.passwordType = this.showPassword ? 'text' : 'password';
+    this.passwordIcon = this.showPassword ? 'visibility_off' : 'visibility';
+  }
 }
