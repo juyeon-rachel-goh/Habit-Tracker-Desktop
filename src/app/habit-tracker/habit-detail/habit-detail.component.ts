9e5c88e4f23fcd653fbfdfa47cf49e204a3dbc08
Rachel Goh <rachelg0129@gmail.com>
2023-01-26 09:35:10 -0500

    Calculation of streaks complete

--- src/app/habit-tracker/habit-detail/habit-detail.component.ts
+++ src/app/habit-tracker/habit-detail/habit-detail.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { Select, Store } from '@ngxs/store';
+import { addDays, format } from 'date-fns';
 import { Observable, catchError, take, tap } from 'rxjs';
+import { DailyHabitRecord } from 'src/app/shared/models/daily-habit-record';
 import { Habit } from 'src/app/shared/models/habit';
 import { HabitService } from 'src/app/shared/services/habit.service';
+import { DailyHabitRecordState } from 'src/app/store/daily-record.state';
 import { DeleteHabit } from 'src/app/store/habit.action';
 import { HabitState } from 'src/app/store/habit.state';
 
@@ -17,6 +20,10 @@ export class HabitDetailComponent implements OnInit {
   public habitId: string = '';
   public currentArchiveStatus: string = '';
   public isArchived!: boolean;
+  public numOfCompletion: number = 0;
+  public currentStreak: number = 0;
+  public bestStreak: number = 0;
+
   constructor(
     private store: Store,
     private route: ActivatedRoute,
@@ -34,6 +41,9 @@ export class HabitDetailComponent implements OnInit {
         this.habitId
       );
     });
+
+    this.findnumOfCompletion(this.habitId);
+    this.findStreaks(this.habitId);
   }
 
   onClickArchive(): void {
@@ -71,6 +81,53 @@ export class HabitDetailComponent implements OnInit {
       return true;
     } else {
       return false;
+    }
+  }
+
+  private findnumOfCompletion(habitId: string) {
+    const count: number =
+      this.store
+        .selectSnapshot(DailyHabitRecordState.dailyCompletionStatus)
+        ?.filter(
+          (record) =>
+            record.habitId === habitId &&
+            new Date(record.date) <= addDays(new Date(), 1) &&
+            record.completionStatus === true
+        ).length || 0;
+    this.numOfCompletion = count;
+  }
+
+  private findStreaks(habitId: string) {
+    const records = this.store
+      .selectSnapshot(DailyHabitRecordState.dailyCompletionStatus)!
+      .filter((record) => record.habitId === habitId);
+    // dateRange to loop = createdOn~today
+    const startDate = new Date(this.habitData!.createdOn);
+    const endDate = new Date();
+
+    let resultArr = [];
+    for (let i = startDate; startDate <= endDate; i.setDate(i.getDate() + 1)) {
+      let date = format(new Date(i), 'MM/dd/yyyy');
+      // find matching date from [records]
+      const matching = records.find((record) => record.date === date);
+      if (matching) {
+        resultArr.push(matching?.completionStatus);
+      } else {
+        resultArr.push(false);
+      }
+    }
+    // Find CURRENT streak
+    const index = resultArr.lastIndexOf(false);
+    const currentStreak = resultArr.slice(index + 1).length;
+    this.currentStreak = currentStreak;
+    // Find BEST streak
+    let valueStr = resultArr.join('');
+    const countTrue = valueStr.split('false').filter(Boolean);
+    if (countTrue.length > 0) {
+      const numOfStreaks = countTrue.map((x) => x.length / 4);
+      this.bestStreak = Math.max(...numOfStreaks);
+    } else {
+      this.bestStreak = 0;
     }
   }
 }
